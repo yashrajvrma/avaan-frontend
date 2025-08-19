a73450315c6466391364e1bc92a902b25a1faa85
yashrajvrma <vermayashraj2005@gmail.com>
2025-08-19 16:28:14 +0530

    added icons on trust section

--- src/components/trust-section.tsx
+++ src/components/trust-section.tsx
@@ -1,30 +1,36 @@
 import { ShieldCheck, Truck, IndianRupee, Clock, MapPin } from "lucide-react";
 // import packageImg from "../../public/images/depositphotos_165317552-stock-photo-cheerful-delivery-man-happy-young.jpg";
 import packageImg from "../../public/images/n9.jpeg";
+import map from "../../public/images/map-marker-check.png";
+import userExp from "../../public/images/userExp.png";
+import money from "../../public/images/money-transfer-smartphone.png";
+import privacy from "../../public/images/privacy-settings.png";
+import truck from "../../public/images/shipping-truck.png";
+import indiaMap from "../../public/images/xcessluggugae_1.png";
 
 const features = [
   {
-    icon: ShieldCheck,
+    icon: privacy,
     text: "Professional handling with real-time tracking.",
     title: "Safe & Secure",
   },
   {
-    icon: Truck,
+    icon: truck,
     text: "We handle everything from pickup to delivery.",
     title: "Hassle-Free",
   },
   {
-    icon: IndianRupee,
+    icon: money,
     text: "Transparent pricing without hidden costs.",
     title: "Affordable",
   },
   {
-    icon: MapPin,
+    icon: map,
     text: "From metro cities to remote towns, we've got you covered.",
     title: "Nationwide Reach",
   },
   {
-    icon: Clock,
+    icon: userExp,
     text: "25+ years of proven logistics expertise.",
     title: "Experience You Can Trust",
   },
@@ -49,15 +55,18 @@ export function TrustSection() {
         {/* Features */}
         <ul className="space-y-6 flex-1">
           {features.map((f) => (
-            <li key={f.text} className="flex items-start gap-4">
-              <div className="flex items-center justify-center rounded-lg bg-yellow-50 text-yellow-500 p-2 flex-shrink-0">
-                <f.icon className="h-8 w-8" />
+            <li key={f.text} className="flex items-center gap-4">
+              <div className="flex items-center justify-center rounded-lg text-yellow-500 p-2 flex-shrink-0">
+                {/* <f.icon className="h-8 w-8" /> */}
+                <img src={f.icon} alt="icon" className="w-16 h-16" />
               </div>
               <div>
-                <h3 className="text-foreground font-semibold text-lg mb-1">
+                <div className="font-semibold text-2xl tracking-tight mb-1">
                   {f.title}
-                </h3>
-                <p className="text-gray-600 font-medium text-base">{f.text}</p>
+                </div>
+                <div className="text-base font-normal text-gray-600">
+                  {f.text}
+                </div>
               </div>
             </li>
           ))}
@@ -66,7 +75,7 @@ export function TrustSection() {
         {/* Image */}
         <div className="flex">
           <img
-            src={packageImg}
+            src={indiaMap}
             alt="Insured shipping card preview"
             className="rounded-3xl border shadow-sm sm:max-w-lg max-w-sm w-full h-96 object-cover"
           />
